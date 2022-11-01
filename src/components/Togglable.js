c815b853a591492eabb9b55edcadf56cd1b3024a
pratiksj <pratiksharai728@gmail.com>
2022-11-01 17:06:32 +0545

    exercise of 3b completed

--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/display-name */
 import { useState, forwardRef, useImperativeHandle } from "react";
+import PropTypes from "prop-types";
 
 const Togglable = forwardRef((props, refs) => {
   const [visible, setVisible] = useState(false);
@@ -29,5 +30,11 @@ const Togglable = forwardRef((props, refs) => {
     </div>
   );
 });
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
+
+Togglable.displayName = "Togglable";
 
 export default Togglable;
