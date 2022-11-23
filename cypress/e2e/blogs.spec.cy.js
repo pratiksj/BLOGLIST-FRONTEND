51767463245fd20dc0d87ee9f2b411616d3c548b
pratiksj <pratiksharai728@gmail.com>
2022-11-23 14:43:53 +0545

    feat: 5.17 testing the application display

--- cypress/e2e/blogs.spec.cy.js
+++ cypress/e2e/blogs.spec.cy.js
@@ -1,11 +1,10 @@
 describe("Blog app", function () {
-  //   beforeEach(function () {
-  //     cy.request("POST", "http://localhost:3003/api/testing/reset");
-  //     cy.visit("http://localhost:3000");
-  //   });
+  beforeEach(function () {
+    cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.visit("http://localhost:3000");
+  });
 
   it("Login form is shown", function () {
-    cy.visit("http://localhost:3000");
     cy.contains("login").click();
   });
 });
