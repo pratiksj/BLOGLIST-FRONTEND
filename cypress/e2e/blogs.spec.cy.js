d55229e8d7d8d73dd81f2f464bbdb11d03277aa6
pratiksj <pratiksharai728@gmail.com>
2022-11-25 09:36:53 +0545

    feat: 5:18 testing both successful and unsuccessful login attempt

--- cypress/e2e/blogs.spec.cy.js
+++ cypress/e2e/blogs.spec.cy.js
@@ -1,10 +1,33 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    const user = {
+      name: "Laxmii",
+      username: "Laxmi",
+      password: "123",
+    };
+    cy.request("POST", "http://localhost:3003/api/users", user);
     cy.visit("http://localhost:3000");
   });
 
   it("Login form is shown", function () {
     cy.contains("login").click();
+  });
+  it("login fails with wrong password", function () {
+    cy.contains("login").click();
+    cy.get("#username").type("Laxmi");
+    cy.get("#password").type("abc");
+    cy.get("#login-button").click();
+
+    cy.contains("wrong username or password");
+  });
+
+  it("user can login", function () {
+    cy.contains("login").click();
+    cy.get("#username").type("Laxmi");
+    cy.get("#password").type("123");
+    cy.get("#login-button").click();
+
+    cy.contains("Laxmii logged in");
   });
 });
